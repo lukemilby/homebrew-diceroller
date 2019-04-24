498583d6d318a3a89dd810d7e73a0e3b90e21dc5
goreleaserbot <goreleaser@carlosbecker.com>
2019-04-24 11:16:33 -0500

    Brew formula update for homebrew-diceroller version v0.1.3

--- diceroller.rb
+++ diceroller.rb
@@ -2,9 +2,9 @@
 class Diceroller < Formula
   desc "Deliver Go binaries as fast and easily as possible"
   homepage "https://derp.com"
-  url "https://github.com/lukemilby/homebrew-diceroller/releases/download/v0.1.3/homebrew-diceroller_0.1.3_Darwin_x86_64.tar.gz", :using => GitHubPrivateRepositoryReleaseDownloadStrategy
+  url "https://github.com/lukemilby/homebrew-diceroller/releases/download/v0.1.3/homebrew-diceroller_0.1.3_Darwin_x86_64.tar.gz"
   version "0.1.3"
-  sha256 "34f3c5d6b0d8cc6a752290b47be36b18ae7ca38915c0ce06d8c77c73c4ad3ef2"
+  sha256 "b56892962fefc8f185aa0671a48081d5492d49947aca4fbf761fcd1e141d46b3"
 
   def install
     bin.install "diceroller"
